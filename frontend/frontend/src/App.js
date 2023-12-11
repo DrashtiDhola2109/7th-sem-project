776531c2d42e528960930b5861b6598cd1a06f39
DrashtiDhola2109 <83170084+DrashtiDhola2109@users.noreply.github.com>
2023-12-11 14:23:28 +0530

    frontend sidebar added

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MiniDrawer from './components/sidebar';
+
+function App() {
+  return (
+    <div className="App">
+
+      <MiniDrawer />
+      
+    </div>
+  );
+}
+
+export default App;
